aebd3748238caabe87ed8c9e3a2f676670c0e9a0
Vadimvill <112726254+Vadimvill@users.noreply.github.com>
2024-02-21 21:48:39 +0300

    Add files via upload

--- src/main/java/com/api/dao/EmailDao.java
+++ src/main/java/com/api/dao/EmailDao.java
@@ -1,7 +1,6 @@
 package com.api.dao;
 
 import jakarta.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
