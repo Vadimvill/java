0948569141763ef823630088a78cda0d36c15310
Vadimvill <112726254+Vadimvill@users.noreply.github.com>
2024-02-21 21:33:08 +0300

    Delete api directory

--- api/src/main/java/com/api/dao/EmailRepository.java
+++
@@ -1,8 +0,0 @@
-package com.api.dao;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmailRepository extends JpaRepository<EmailEntity, Long> {
-
-}
