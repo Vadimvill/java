f2bc8b46f72301bd9d986e681bcd46550eed8ee9
Vadimvill <112726254+Vadimvill@users.noreply.github.com>
2024-02-21 21:32:18 +0300

    Add files via upload

---
+++ api/src/main/java/com/api/controller/DTO.java
@@ -0,0 +1,17 @@
+package com.api.controller;
+
+public class DTO {
+    String text;
+
+    public DTO(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
