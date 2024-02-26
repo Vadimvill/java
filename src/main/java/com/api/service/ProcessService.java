27b598e57f92c840417d713a22bde0ca6a890196
Vadimvill <112726254+Vadimvill@users.noreply.github.com>
2024-02-26 09:32:15 +0300

    Add files via upload

--- src/main/java/com/api/service/ProcessService.java
+++ src/main/java/com/api/service/ProcessService.java
@@ -14,30 +14,32 @@ import java.util.regex.Pattern;
 @ComponentScan(basePackages = "com.api.dao")
 @EnableJpaRepositories(basePackages = "com.api.dao")
 @EntityScan("com.api.entity")
-public class ProcessService{
+public class ProcessService {
     private final EmailRepository emailRepository;
-    public ProcessService(EmailRepository emailRepository){
+
+    public ProcessService(EmailRepository emailRepository) {
         this.emailRepository = emailRepository;
     }
-public String getSecureText(String text){
-    List<String> list = new ArrayList<>();
-    String phoneRegex = "\\b(?:\\+\\d{1,3}[-.\\s]?)?(\\d{1,4}[-.\\s]?){1,2}\\d{1,9}\\b";
-    Pattern phonePattern = Pattern.compile(phoneRegex);
-    Matcher phoneMatcher = phonePattern.matcher(text);
-    text = phoneMatcher.replaceAll("");
 
-    String emailRegex = "\\b[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,}\\b";
-    Pattern emailPattern = Pattern.compile(emailRegex, Pattern.CASE_INSENSITIVE);
-    Matcher emailMatcher = emailPattern.matcher(text);
-    
-    while (emailMatcher.find()){
-        list.add(emailMatcher.group());
-    }
-    text = emailMatcher.replaceAll("");
-    for(int i = 0;i<list.size();i++){
-        emailRepository.save(new EmailEntity(list.get(i)));
-    }
-    return text;
+    public String getSecureText(String text) {
+        List<String> list = new ArrayList<>();
+        String phoneRegex = "\\b(?:\\+\\d{1,3}[-.\\s]?)?(\\d{1,4}[-.\\s]?){1,2}\\d{1,9}\\b";
+        Pattern phonePattern = Pattern.compile(phoneRegex);
+        Matcher phoneMatcher = phonePattern.matcher(text);
+        text = phoneMatcher.replaceAll("");
 
-  }
+        String emailRegex = "\\b[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,}\\b";
+        Pattern emailPattern = Pattern.compile(emailRegex, Pattern.CASE_INSENSITIVE);
+        Matcher emailMatcher = emailPattern.matcher(text);
+
+        while (emailMatcher.find()) {
+            list.add(emailMatcher.group());
+        }
+        text = emailMatcher.replaceAll("");
+        for (int i = 0; i < list.size(); i++) {
+            emailRepository.save(new EmailEntity(list.get(i)));
+        }
+        return text;
+
+    }
 }
