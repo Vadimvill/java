484cfcf487a0d01f68d8d4a2a785720c31df7f46
Vadimvill <112726254+Vadimvill@users.noreply.github.com>
2024-03-25 09:49:34 +0300

    Add files via upload

--- src/main/java/com/api/component/Cache.java
+++ src/main/java/com/api/component/Cache.java
@@ -2,7 +2,7 @@ package com.api.component;
 
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @Component
@@ -10,7 +10,7 @@ public class Cache {
     private Map<String, Object> hashMap;
 
     public Cache() {
-        this.hashMap = new HashMap<>();
+        this.hashMap = new LinkedHashMap<>();
     }
 
     public Object get(String key) {
@@ -19,6 +19,10 @@ public class Cache {
 
     public void put(String key, Object obj) {
         hashMap.put(key, obj);
+        if (hashMap.size() > 10) {
+            String oldKey = hashMap.keySet().iterator().next();
+            hashMap.remove(oldKey);
+        }
     }
 
     public void remove(String key) {
