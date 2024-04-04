ad5a5a871fe3e282c6e105f0900bd85561bc3675
Vadimvill <112726254+Vadimvill@users.noreply.github.com>
2024-04-04 20:34:01 +0300

    Add files via upload

--- src/main/test/java/com/api/service/EmailServiceTest.java
+++ src/main/test/java/com/api/service/EmailServiceTest.java
@@ -4,13 +4,19 @@ import com.api.component.Cache;
 import com.api.component.CustomLogger;
 import com.api.dao.EmailRepository;
 import com.api.dao.EmailTypeRepository;
+import com.api.dto.EmailDTO;
+import com.api.entity.Email;
+import com.api.entity.EmailType;
+import com.api.exceptions.ServiceException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+
+import org.mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 class EmailServiceTest {
 
@@ -35,19 +41,20 @@ class EmailServiceTest {
     }
 
     @Test
-    void getConfidentinalText(){
-        String text1 = "thinde@mail.ru test +375202556867 ms";
-        String text2 = "t+375202556867 ms";
-        String text3 = "thinde@mail.ru ms";
-        String text4 = "teterte@mail.com";
-        String result1 = emailService.getConfidentialText(text1);
-        String result2 = emailService.getConfidentialText(text2);
-        String result3 = emailService.getConfidentialText(text3);
-        String result4 = emailService.getConfidentialText(text4);
-        Assertions.assertEquals(" test + ms",result1);
-        Assertions.assertEquals("t ms",result2);
-        Assertions.assertEquals(" ms",result3);
-        Assertions.assertEquals("",result4);
+    void shouldReturnAllEmails() {
+        Mockito.when(emailRepository.findAll()).thenReturn(getEmails());
+        List<EmailDTO> res = emailService.getEmails("all");
+        Assertions.assertEquals(3,res.size());
+        Assertions.assertNotNull(res.get(0));
     }
 
+    private List<Email> getEmails(){
+        List<Email> list = new ArrayList<>();
+        list.add(new Email(1L,"vafda@gmail.com"));
+        list.add(new Email(2L,"vafrteda@mail.com"));
+        list.add(new Email(3L,"vafda@rambler.ru"));
+        return list;
+    }
 }
+
+
